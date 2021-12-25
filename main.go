efe878900c3019e5d57b251f0c42efe4f7901a1f
vicxzhang <vicxzhang@mail.ustc.edu.cn>
2021-12-25 17:27:56 +0800

    12-25-17:27

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func setupLogger() error {
 
 func initClient() (*redis.Client, error) {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "redis-cn02zljq32jffvirx.redis.volces.com:6379",
+		Addr:     "redis-cn02zljq32jffvirx.redis.ivolces.com:6379",
 		Password: "dmp_group2",
 		DB:       0,
 		PoolSize: 10,
@@ -61,22 +61,22 @@ func initClient() (*redis.Client, error) {
 	return rdb, nil
 }
 
-func addToRedis(finame string) error {
+func addToRedis(filename string) error {
 	// defer wg.Done()
 
-	log.Infof("crowd %s starts to sync to redis", finame)
+	log.Infof("crowd %s starts to sync to redis", filename)
 
 	rdb, err := initClient()
 	if err != nil {
-		log.Fatalf("Redis connect error", err)
+		log.Fatalf("Redis connect error: %s", err)
 	}
 	defer rdb.Close()
 
 	pipe := rdb.Pipeline()
 	defer pipe.Close()
 
-	cfile := datadir + "/" + finame
-	file, err := os.Open(cfile)
+	crowdfile := datadir + "/" + filename
+	file, err := os.Open(crowdfile)
 	if err != nil {
 		log.Fatalf("open file fail: %s", err)
 	}
@@ -92,22 +92,22 @@ func addToRedis(finame string) error {
 		if iterator == 10000 {
 			_, err := pipe.Exec(context.Background())
 			if err != nil {
-				log.Fatalf("%s pipeline error:%s", finame, err)
+				log.Fatalf("%s pipeline error:%s", filename, err)
 			}
-			// log.Infof("%s add to redis: %d", finame, len(cmds))
+			// log.Infof("%s add to redis: %d", filename, len(cmds))
 			iterator = 0
 		}
 	}
 	if iterator != 0 {
 		_, err := pipe.Exec(context.Background())
 		if err != nil {
-			log.Fatalf("%s pipeline error:%s", err)
+			log.Fatalf("%s pipeline error: %s", filename, err)
 		}
-		// log.Infof("%s add to redis: %d", finame, len(cmds))
+		// log.Infof("%s add to redis: %d", filename, len(cmds))
 		iterator = 0
 	}
 
-	log.Infof("crowd %s synced to redis", finame)
+	log.Infof("crowd %s synced to redis", filename)
 
 	return nil
 }
